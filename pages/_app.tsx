18c42fa73eb92c890c7edcd819e809c71c8d3b26
Nathan Fargo <32229490+ntfargo@users.noreply.github.com>
2023-02-11 07:15:09 +0100

    components\cardcss:   components/card/card.module.css 	components\card:   components/card/index.tsx 	modified:   package.json 	clomdx:   pages/CLOUD_Service.mdx 	gitmdx:   pages/GIT_Service.mdx 	modified:   pages/V1_API/users.mdx 	modified:   pages/_app.tsx 	modified:   pnpm-lock.yaml 	modified:   tsconfig.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement } from "react";
 import type { AppProps } from "next/app";
 
-import "../styles/global.css";
+import "../styles/global.css"; 
 
 export default function Nextra({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />;
